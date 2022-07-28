90e0c725b1b4b80422484bba15be4dbbb27bf9af
Crowdbotics <team@crowdbotics.com>
2022-07-28 21:14:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/my_360_protection_36474/MainActivity.java
@@ -0,0 +1,15 @@
+package com.my_360_protection_36474;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "my_360_protection_36474";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "my_360_protection_36474",
+  url: "https://my_360_protection_36474.botics.co",
+  api: "https://my_360_protection_36474.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
